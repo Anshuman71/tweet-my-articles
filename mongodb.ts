f5e40f8ca6884df266a0e2a7578cefeda38c6715
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-01-21 15:38:26 +0530

    fix try catch

--- mongodb.ts
+++ mongodb.ts
@@ -5,26 +5,20 @@ import { generateLogString } from "./utils";
 let cachedDB: Db | null = null;
 
 // A function for connecting to MongoDB,
-export default async function connectToDatabase(): Promise<Db | null> {
+export default async function connectToDatabase(): Promise<Db> {
   // If the database connection is cached, use it instead of creating a new connection
   if (cachedDB) {
     console.info(generateLogString("Using cached client!"));
     return cachedDB;
   }
-  try {
-    const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    };
-    console.info(generateLogString("No client found! Creating a new one."));
-    // @ts-ignore If no connection is cached, create a new one
-    const client = new MongoClient(process.env.ATLAS_URI_PROD as string, opts);
-    const db: Db = client.db(process.env.DB_NAME);
-    cachedDB = db;
-    return cachedDB;
-  } catch (error) {
-    console.debug(generateLogString("Error while creating connection!"));
-    console.error(error);
-    return null;
-  }
+  const opts = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  console.info(generateLogString("No client found! Creating a new one."));
+  // @ts-ignore If no connection is cached, create a new one
+  const client = new MongoClient(process.env.ATLAS_URI_PROD as string, opts);
+  const db: Db = client.db(process.env.DB_NAME);
+  cachedDB = db;
+  return cachedDB;
 }
