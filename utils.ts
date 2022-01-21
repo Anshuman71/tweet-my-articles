fa00996dbe75db7fb22aabe61017781a7d3d53ad
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-01-21 13:44:38 +0530

    Add utils for createDevArticle

--- utils.ts
+++ utils.ts
@@ -1,3 +1,5 @@
+import { Article, DevArticle, SOURCE } from "./types";
+
 export async function getPublishedArticlesFromDEV() {
   const res = await fetch(`${process.env.DEV_API_URL}/articles/me/published`, {
     headers: {
@@ -5,4 +7,15 @@ export async function getPublishedArticlesFromDEV() {
     },
   });
   return res.json();
+}
+export function createDevArticle(article: DevArticle): Article {
+  return {
+    id: article.id,
+    source: SOURCE.dev,
+    title: article.title,
+    published_at: new Date(article.published_timestamp).getTime(),
+    lastViewsMilestone: 0,
+    lastReactionsMilestone: 0,
+    lastTweetedAt: Date.now(),
+  };
 }
