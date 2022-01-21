f54a8dc44bfa651b132033078ddbb0bf6bea2bc9
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-01-21 14:02:42 +0530

    add generateLogString

--- utils.ts
+++ utils.ts
@@ -19,3 +19,9 @@ export function createDevArticle(article: DevArticle): Article {
     lastTweetedAt: Date.now(),
   };
 }
+
+export function generateLogString(val: string) {
+  const separator = "=".repeat(20);
+  const padding = " ".repeat(10);
+  return separator + padding + val + padding + separator;
+}
