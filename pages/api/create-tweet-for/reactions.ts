5945c740b16f5587311d091c9aaf3027bf17559c
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-01-21 14:02:53 +0530

    Add console info

--- pages/api/create-tweet-for/reactions.ts
+++ pages/api/create-tweet-for/reactions.ts
@@ -1,6 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import connectToDatabase from "../../../mongodb";
-import { createDevArticle, getPublishedArticlesFromDEV } from "../../../utils";
+import {
+  createDevArticle,
+  generateLogString,
+  getPublishedArticlesFromDEV,
+} from "../../../utils";
 import { COLLECTION_NAMES, DevArticle, SOURCE } from "../../../types";
 import { REACTIONS_MILESTONE_SEQUENCE } from "../../../constants";
 
@@ -8,14 +12,18 @@ export default async function reactions(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
+  console.info(generateLogString("Running Reactions Function"));
   try {
     const devArticles = await getPublishedArticlesFromDEV();
+    console.info(generateLogString("Total Articles: " + devArticles.length));
     const database = await connectToDatabase();
     const articlesCollection = database.collection(COLLECTION_NAMES.articles);
     const devArticleFromDB = await articlesCollection
       .find({ source: SOURCE.dev })
       .toArray();
-
+    console.info(
+      generateLogString("Total Articles In DB: " + devArticleFromDB.length)
+    );
     devArticles.forEach(async (article: DevArticle) => {
       const findExpression = {
         id: article.id,
@@ -26,19 +34,27 @@ export default async function reactions(
         const milestoneReached = REACTIONS_MILESTONE_SEQUENCE.find(
           (milestone) => article.public_reactions_count > milestone
         );
+        console.info(
+          generateLogString("Milestone reached: " + milestoneReached)
+        );
+        console.info(
+          generateLogString("Existing Milestone: " + value.lastViewsMileStone)
+        );
         if (
           milestoneReached &&
           milestoneReached !== value.lastReactionsMilestone
         ) {
+          console.info(generateLogString("Updating database"));
           await articlesCollection.updateOne(findExpression, {
             $set: {
               lastReactionsMilestone: milestoneReached,
-              last_tweeted_at: Date.now(),
+              lastTweetedAt: Date.now(),
             },
           });
           // send tweet
         }
       } else {
+        console.info(generateLogString("New article found: " + article.title));
         await articlesCollection.insertOne(createDevArticle(article));
       }
     });
--- pages/api/create-tweet-for/views.ts
+++ pages/api/create-tweet-for/views.ts
@@ -1,6 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import connectToDatabase from "../../../mongodb";
-import { createDevArticle, getPublishedArticlesFromDEV } from "../../../utils";
+import {
+  createDevArticle,
+  generateLogString,
+  getPublishedArticlesFromDEV,
+} from "../../../utils";
 import { COLLECTION_NAMES, DevArticle, SOURCE } from "../../../types";
 import { VIEWS_MILESTONE_SEQUENCE } from "../../../constants";
 
@@ -8,13 +12,18 @@ export default async function views(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
+  console.info(generateLogString("Running Views Function"));
   try {
     const devArticles = await getPublishedArticlesFromDEV();
+    console.info(generateLogString("Total Articles: " + devArticles.length));
     const database = await connectToDatabase();
     const articlesCollection = database.collection(COLLECTION_NAMES.articles);
     const devArticleFromDB = await articlesCollection
       .find({ source: SOURCE.dev })
       .toArray();
+    console.info(
+      generateLogString("Total Articles In DB: " + devArticleFromDB.length)
+    );
 
     devArticles.forEach(async (article: DevArticle) => {
       const findExpression = {
@@ -26,7 +35,14 @@ export default async function views(
         const milestoneReached = VIEWS_MILESTONE_SEQUENCE.find(
           (milestone) => article.page_views_count > milestone
         );
+        console.info(
+          generateLogString("Milestone reached: " + milestoneReached)
+        );
+        console.info(
+          generateLogString("Existing Milestone: " + value.lastViewsMileStone)
+        );
         if (milestoneReached && milestoneReached !== value.lastViewsMileStone) {
+          console.info(generateLogString("Updating database"));
           await articlesCollection.updateOne(findExpression, {
             $set: {
               lastViewsMileStone: milestoneReached,
@@ -36,6 +52,7 @@ export default async function views(
           // send tweet
         }
       } else {
+        console.info(generateLogString("New article found: " + article.title));
         await articlesCollection.insertOne(createDevArticle(article));
       }
     });
