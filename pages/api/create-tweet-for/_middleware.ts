bd483720389544ffd48e6ad0163b25c69d7a4c56
Anshuman Bhardwaj <anshu5074@gmail.com>
2022-01-24 16:40:59 +0530

    fix middleware security

--- pages/api/create-tweet-for/_middleware.ts
+++ pages/api/create-tweet-for/_middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const apiKey = request.headers.get("api-key");
   console.info("========== Running middleware ============");
-  if (request.method !== "POST" && apiKey !== process.env.API_KEY) {
+  if (request.method !== "POST" || apiKey !== process.env.API_KEY) {
     return NextResponse.json({
       type: "Error",
       code: 405,
